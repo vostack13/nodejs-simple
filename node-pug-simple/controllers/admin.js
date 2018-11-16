543fbf39a067de5bc863367de255665aa2b207e2
vostack13 <stanislav.vozikov@gmail.com>
2018-11-16 09:27:10 +0300

    update: clear repo

--- node-pug-simple/controllers/admin.js
+++
@@ -1,3 +0,0 @@
-module.exports.getAdminPage = function(req, res) {
-  res.render('index', { title: 'Личный кабинет' });
-};
--- node-pug-simple/controllers/blog.js
+++
@@ -1,3 +0,0 @@
-module.exports.getBlogPage = function(req, res) {
-  res.render('index', { title: 'Блог' });
-};
