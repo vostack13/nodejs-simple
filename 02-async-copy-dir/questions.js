1ca732dba0f78688bed398e9de5c8322a01e96a5
vostack13 <stanislav.vozikov@gmail.com>
2018-11-21 20:43:20 +0300

    Add: Done homework №2-2(async)

---
+++ 02-async-copy-dir/questions.js
@@ -0,0 +1,51 @@
+exports.run = [
+  {
+    type: 'list',
+    name: 'runSort',
+    message: 'Хочешь разложить файлы каталога в по алфавиту?',
+    choices: ['Да', 'Нет']
+  },
+  {
+    type: 'list',
+    name: 'isSourcePath',
+    message: 'Какой возьмем каталог?',
+    choices: ['Уже заготовленный', 'Ввести свой путь'],
+    when: function (answers) {
+      return answers.runSort === 'Да';
+    }
+  },
+  {
+    type: 'input',
+    name: 'sourcePath',
+    message: 'Введите адресс своего каталога:',
+    when: function (answers) {
+      return answers.isSourcePath === 'Ввести свой путь';
+    }
+  },
+  {
+    type: 'list',
+    name: 'isOutPath',
+    message: 'Где расположить файлы?',
+    choices: ['В корне данной директории', 'Ввести свой путь'],
+    when: function (answers) {
+      return answers.runSort === 'Да';
+    }
+  },
+  {
+    type: 'input',
+    name: 'outPath',
+    message: 'Введите путь каталога, в котором расположить отсортированные файлы:',
+    when: function (answers) {
+      return answers.isOutPath === 'Ввести свой путь';
+    }
+  },
+  {
+    type: 'list',
+    name: 'removeDir',
+    message: 'Удалить после сортировки исходный каталог?',
+    choices: ['Не удалять', 'Удалить'],
+    when: function (answers) {
+      return answers.runSort === 'Да';
+    }
+  }
+];
